4113600af70772545467d186e0c906b83e853e9c
pacifistes <bbrunell@student.42.fr>
2017-03-21 21:40:37 +0100

    Update buff_free_njoin.c

--- srcs/buff_free_njoin.c
+++ srcs/buff_free_njoin.c
@@ -24,16 +24,14 @@ void	buff_free_njoin(char **str, t_buffer *b, int nbr_str, int *str_free)
 	while (++i < nbr_str)
 		if (str_free[i] == 1)
 			nbr_free++;
+	j = -1;
 	tmp = (char**)malloc(sizeof(char*) * (nbr_free + 1));
 	i = -1;
 	while (++i < nbr_str)
 		while (str[i] && i < nbr_str)
 		{
-			if (str_free[i] == 1)
-			{
+			if (str_free[i] == 1 && ++j < nbr_str)
 				tmp[j] = str[i];
-				j++;
-			}
 			buff_join(str[i], b);
 			i++;
 		}
